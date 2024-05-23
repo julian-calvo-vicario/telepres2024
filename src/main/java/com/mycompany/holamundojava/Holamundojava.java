7d4e80739f8844991aee6479aa4d0da80a2dfb76
EC2 Default User <ec2-user@ip-172-31-32-105.eu-north-1.compute.internal>
2024-05-23 17:37:04 +0000

    feature 1: initial comit

---
+++ src/main/java/com/mycompany/holamundojava/Holamundojava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.holamundojava;
+
+/**
+ *
+ * @author ec2-user
+ */
+public class Holamundojava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
